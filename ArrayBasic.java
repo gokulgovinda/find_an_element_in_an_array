c091a19080d20ad346c005a6fcf9911993846e7b
Gokul Govinda <87459046+gokulgovinda@users.noreply.github.com>
2022-10-17 16:32:09 +0530

    Update ArrayBasic.java

--- ArrayBasic.java
+++ ArrayBasic.java
@@ -5,7 +5,7 @@ public class arrayBasics {
 	public static void main(String[] args) {
 		int a[] = {2, 1, 4, 6, 5};
 		int  key = 6;
-		
+		 
 		int result = -1; 
 		// search for k
 		for(int i= 0; i<a.length; i++) {
